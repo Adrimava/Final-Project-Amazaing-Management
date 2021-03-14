6a99e7ef52a06b15fa905fa0830a0eff51bcab19
Adrián Maza <adri_mv1@hotmail.com>
2021-03-14 17:17:37 +0100

    folder organization

--- backend/amazaing-management/src/main/java/com/ironhack/finalproject/amazaingmanagement/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ironhack.finalproject.amazaingmanagement.repository;
-
-import com.ironhack.finalproject.amazaingmanagement.model.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-}
--- backend/amazaing-management/src/main/java/com/ironhack/finalproject/amazaingmanagement/repository/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ironhack.finalproject.amazaingmanagement.repository;
-
-import com.ironhack.finalproject.amazaingmanagement.model.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-}
